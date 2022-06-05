a9a24178a6c7a1905d3b1fbab5fadd0e3c19a512
Justyna Marciniak <justy.marciniak@gmail.com>
2022-06-05 21:59:34 +0900

    add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import './index.css';
+import ImageCarousel from './components/ImageCarousel/ImageCarousel';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className='wrapper'>
+      <ImageCarousel />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CarouselDescription/CarouselDescription.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CarouselDescription.css';
+
+const CarouselDescription = () => {
+  return (
+    <div className='description'>
+      <h1 className='title'>A space where designers are created</h1>
+      <p className='text--paragraph'>
+        A space where designers are created with people facing various projects
+        <br />
+        This space was created based on the concept of viewing the office as a
+        city and designing the experience of that city
+      </p>
+      <div className='description--bg'></div>
+    </div>
+  );
+};
+
+export default CarouselDescription;
